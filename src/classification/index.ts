775951fdf703e7e5b8f65bf39b9ba56a6c139659
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-18 15:50:10 -0700

    AI: export classifier boundary

---
+++ src/classification/index.ts
@@ -0,0 +1,15 @@
+export { ConservativeStopClassifier } from "./classifier.js";
+export { sanitizeContext, redactSecrets, type ContextSanitizerOptions } from "./context.js";
+export { evaluateDeterministicRules } from "./rules.js";
+export {
+  MAX_REASON_LENGTH,
+  boundedReason,
+  unsureResult,
+  type ClassificationDecision,
+  type ClassificationReasonCode,
+  type ClassificationRequest,
+  type ClassificationResult,
+  type ConversationTurn,
+  type SanitizedContext,
+  type SanitizedTurn,
+} from "./types.js";
